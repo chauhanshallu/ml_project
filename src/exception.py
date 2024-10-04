ffa027f6817f58b331df20b3d451ce0c72b9c53a
chauhanshallu <shalluchauhan1307@gmail.com>
2024-10-04 13:33:50 +0200

    EDA and Problem statement

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -11,7 +11,7 @@ def error_message_detail(error,error_detail:sys):
 
 
 
-class CustomeException(Exception):
+class CustomException(Exception):
     def __init__(self, error_message,error_detail:sys):
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
@@ -20,5 +20,10 @@ class CustomeException(Exception):
         return self.error_message
     
 
+if __name__=="__main__":
 
-    
+        try:
+            a=1/0
+        except Exception as e:
+            logging.info("Divide by Zero")
+            raise CustomException(e,sys)
